58a2753850eab6d3a48bdb77eeefbb7e09296800
Casstiel <cpss5433@gmail.com>
2023-12-01 22:03:25 -0800

    signup and login functionalities done

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose')
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser')
+const users = require('./routes/api/users')
+const User = require('./models/User')
+
 mongoose
-    .connect(db, {useNewUrlParser: true})
+    .connect(db)
     .then(()=>console.log("Connected to mongoDB successfully"))
     .catch(err=> console.log(err))
 
-    app.get('/', (req, res)=> res.send("Hello world"));
+
+
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+
+
+    app.get('/', (req, res)=>{
+        // const user = new User ({
+        //     handle: "casstiel",
+        //     email: 'casstiel@casstiel.cass',
+        //     password: "casstiel123456"
+        // })
+        // user.save()
+        res.send("Hello World!")
+    } );
+
+
+    app.use('/api/users', users)
+
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
